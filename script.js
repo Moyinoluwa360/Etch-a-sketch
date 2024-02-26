9cea91847ca3cfe9e0e136df68be9d79f388e982
Moyinoluwa360 <122643939+Moyinoluwa360@users.noreply.github.com>
2024-02-26 18:11:33 +0100

    Add various functionalities to the button created but not done with the shading button

--- script.js
+++ script.js
@@ -13,9 +13,8 @@ function getGrid(dimension){
         }
       }
 }
-getGrid(50)
-let isClicked = false
 function draw(){
+    let isClicked = false
     let rowItemList = document.querySelectorAll(".rowItem");
     for (let rowItemListCounter = 0; rowItemListCounter < rowItemList.length; rowItemListCounter++){
         rowItemList[rowItemListCounter].addEventListener("mousedown",() => {
@@ -32,4 +31,147 @@ function draw(){
         })
 }
 }
+// random color function
+function RGBcolor() {
+    let R = Math.floor(Math.random() * 256);
+    let G = Math.floor(Math.random() * 256);
+    let B = Math.floor(Math.random() * 256);
+    let randomcolor = "rgb(" + R + "," + G + "," + B + ")";  
+    return randomcolor
+  }
+
+getGrid(16)
 draw()
+// event listener for Grid Number
+const gridlayout = document.querySelector("#dimensions")
+gridlayout.addEventListener("click",() => {
+    let gridNum = prompt("Enter the number of dimension you want your grid. Note not to exceed 100");
+    if (isNaN(gridNum)){
+        alert("Make sure input is a number")
+    }else{
+        gridNum = Number(gridNum)
+        if (gridNum > 100){
+            gridNum = 100 ;
+        }
+        const rowDelete = document.querySelectorAll(".row")
+        const juniorContainer = document.querySelector("#juniorContainer")
+        let lenRowDelete = rowDelete.length
+        for (let Counter = 0;  Counter < lenRowDelete; Counter++) { 
+            juniorContainer.removeChild(rowDelete[Counter])
+    }
+        const rowItemArr = document.querySelectorAll(".rowItem")
+        let lenRowItemArr = rowItemArr.length
+        for (let counter = 0;  counter < lenRowItemArr; counter++) { 
+            juniorContainer.appendChild(rowItemArr[counter])
+    }
+        getGrid(gridNum)
+        draw()
+    }
+}
+)
+// event listener for Togle Border
+const borderToogle = document.querySelector("#borderToogle");
+let isBorder = true;
+borderToogle.addEventListener("click",() => {
+    if (isBorder){
+        isBorder = false;
+        rowItemBorder = document.querySelectorAll(".rowItem");
+        for(let counter = 0; counter < rowItemBorder.length; counter++){
+            rowItemBorder[counter].style.border = 0 ;
+    }
+    }else{
+        isBorder = true;
+        rowItemBorder = document.querySelectorAll(".rowItem");
+        for(let counter = 0; counter < rowItemBorder.length; counter++){
+            rowItemBorder[counter].style.borderBottom = "0.1px solid black" ;
+            rowItemBorder[counter].style.borderRight = "0.1px solid black" ;
+    }
+    }
+})
+// event listener for eraser
+const eraser = document.querySelector("#eraser");
+let isNotErased = true
+eraser.addEventListener("click",() => {
+    let isClicked = false
+    let rowItemList = document.querySelectorAll(".rowItem");
+    if (isNotErased){
+        isNotErased = false
+        for (let rowItemListCounter = 0; rowItemListCounter < rowItemList.length; rowItemListCounter++){
+            rowItemList[rowItemListCounter].addEventListener("mousedown",() => {
+                isClicked = true
+                rowItemList[rowItemListCounter].style.backgroundColor = "white" ;
+            })
+            rowItemList[rowItemListCounter].addEventListener("mouseover",() => {
+                if (isClicked){
+                    rowItemList[rowItemListCounter].style.backgroundColor = "white" ;
+                }
+            })
+            rowItemList[rowItemListCounter].addEventListener("mouseup",() => {
+                isClicked = false
+            })
+    }
+    }else{
+        isNotErased = true
+        draw()
+    }
+})
+// event listener for board clearing
+const boardClear = document.querySelector("#clear")
+boardClear.addEventListener("click", () => {
+    let rowItemList = document.querySelectorAll(".rowItem");
+    for (let rowItemListCounter = 0; rowItemListCounter < rowItemList.length; rowItemListCounter++){
+        rowItemList[rowItemListCounter].style.backgroundColor = "white";
+
+    }
+    draw()
+})
+
+// event listener for rainbow mode
+
+const rainbowMode = document.querySelector("#rainbow")
+let isNotRainbow = true
+rainbowMode.addEventListener("click", () => {
+    let isClicked = false
+    let rowItemList = document.querySelectorAll(".rowItem");
+    if (isNotRainbow){
+        isNotRainbow = false
+        for (let rowItemListCounter = 0; rowItemListCounter < rowItemList.length; rowItemListCounter++){
+            rowItemList[rowItemListCounter].addEventListener("mousedown",() => {
+                isClicked = true
+                rowItemList[rowItemListCounter].style.backgroundColor = RGBcolor() ;
+            })
+            rowItemList[rowItemListCounter].addEventListener("mouseover",() => {
+                if (isClicked){
+                    rowItemList[rowItemListCounter].style.backgroundColor = RGBcolor() ;
+                }
+            })
+            rowItemList[rowItemListCounter].addEventListener("mouseup",() => {
+                isClicked = false
+            })
+    }
+    }else{
+        isNotRainbow = true
+        draw()
+    }
+})
+// eventlistener for shading
+let clicked = 0
+const shader = document.querySelector("#shadingEffect")
+shader.addEventListener("click",() => {
+    let rowItemList = document.querySelectorAll(".rowItem");
+    let isClicked = false
+    for (let rowItemListCounter = 0; rowItemListCounter < rowItemList.length; rowItemListCounter++){
+        rowItemList[rowItemListCounter].addEventListener("mousedown",() => {
+            isClicked = true
+            rowItemList[rowItemListCounter].style.backgroundColor = "black" ;
+        })
+        rowItemList[rowItemListCounter].addEventListener("mouseover",() => {
+            if (isClicked){
+                rowItemList[rowItemListCounter].style.backgroundColor = "black" ;
+            }
+        })
+        rowItemList[rowItemListCounter].addEventListener("mouseup",() => {
+            isClicked = false
+        })
+}
+})
